7a821b01a7b244f2df92a4bebb82b8aa7f7bdd07
Suryansh <heresuryanshsingh@gmail.com>
2025-12-21 17:09:02 +0530

    logger learn up

--- src/main/java/com/cool/springboot/myfirstwebapp/logincontroller/LoginController.java
+++ src/main/java/com/cool/springboot/myfirstwebapp/logincontroller/LoginController.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.ModelMap;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 @Controller
 public class LoginController {
+    // private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @GetMapping("/login")
     public String login(@RequestParam(required = false) String name,
             @RequestParam(required = false) String age,
             @RequestParam(required = false) String birthday,
             ModelMap model) {
+        // logger.debug("name: {}", name);
+        // logger.info("age: {}", age);
+        // logger.info("birthday: {}", birthday);
         model.put("name", name);
         model.put("age", age);
         model.put("birthday", birthday);
